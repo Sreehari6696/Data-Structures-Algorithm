b769403c9d386b1e34071d2901ea066677cd6e57
SREEHARI.S <168545223+Sreehari6696@users.noreply.github.com>
2026-07-31 16:14:52 +0530

    Create digit counting script
    Add a script to count the number of digits in an input number.

---
+++ Counting the number of digits.py
@@ -0,0 +1,6 @@
+# This code finds out the number of digits that are present in the number given as the input
+# Example 1: Input = 345   -> Output  = 3
+# Example 2: Input = 89009 -> Output = 5
+
+N = int(input("Enter the number\n"))
+print("The number of digits of the entered number is = ", len(str(N)))
